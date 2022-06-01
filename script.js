03f7141517e903a5f64093109a0b5ffbb1bac463
HorsebowMoon <yesitsmeer@gmail.com>
2022-06-01 12:46:14 -0400

    Add gamePlay function

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 function computerPlay() {
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
-        return "Rock";
+        return "rock";
     } else if (randomNumber === 1) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
+function gamePlay(playerSelection, computerSelection) {
+    computerSelection = computerPlay();
+    playerSelection = "rock";
+    /*playerSelection = prompt("Rock, paper, scissors: ");*/
+    /*playerSelection = playerSelection.toLowerCase();*/
+    
+    if (computerSelection === "scissors") {
+        return "You win! Rock beats scissors";
+    } else if (computerSelection == "paper") {
+        return "You lose! Paper beats rock!";
+    } else {
+        return "You tie. Both picked rock";
+    }
+}
 
-
-console.log(computerPlay());
+console.log(gamePlay());
