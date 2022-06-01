94be06562a764d89dff664430bccb9f61629b321
HorsebowMoon <yesitsmeer@gmail.com>
2022-06-01 13:10:36 -0400

    Add play function

--- script.js
+++ script.js
@@ -12,16 +12,43 @@ function computerPlay() {
 function gamePlay(playerSelection, computerSelection) {
     computerSelection = computerPlay();
     playerSelection = "rock";
+    let result = ""
     /*playerSelection = prompt("Rock, paper, scissors: ");*/
     /*playerSelection = playerSelection.toLowerCase();*/
     
     if (computerSelection === "scissors") {
-        return "You win! Rock beats scissors";
+        return result = "You win! Rock beats scissors";
     } else if (computerSelection == "paper") {
-        return "You lose! Paper beats rock!";
+        return result = "You lose! Paper beats rock!";
     } else {
-        return "You tie. Both picked rock";
+        return result = "You tie. Both picked rock";
     }
 }
 
-console.log(gamePlay());
+function game() {
+    let playerWin = 0
+    let computerWin = 0
+    for (let i = 0; i < 5; i++) {
+        let result = gamePlay()
+        console.log(result);
+        if (result === "You win! Rock beats scissors") {
+            playerWin += 1;
+        } else if ( result === "You lose! Paper beats rock!") {
+            computerWin += 1;
+        } else {
+            console.log("Nobody won this round");
+        }
+        console.log("You have played " + (i+1) + " round(s)");
+        console.log("\n")
+    }
+
+    if (playerWin < computerWin) {
+        console.log("You lost. The computer won " + computerWin + " times. You only won " + playerWin + " times.");
+    } else if (playerWin > computerWin) {
+        console.log("You won! You had " + playerWin + " victories. The computer won " + computerWin + " times.");
+    } else {
+        console.log("You tied against the computer. You won " + playerWin + " matches and the computer won " + computerWin + " matches" )
+    };
+}
+
+console.log(game());
