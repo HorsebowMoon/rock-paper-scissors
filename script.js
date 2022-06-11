fd036607a6485cfa5086c47cce3c6b29a980a559
HorsebowMoon <yesitsmeer@gmail.com>
2022-06-11 13:11:07 -0400

    Finish recoding for RPS using javascript

--- script.js
+++ script.js
@@ -9,48 +9,104 @@ function computerPlay() {
     }
 }
 
-function gamePlay(playerSelection, computerSelection) {
-    computerSelection = computerPlay();
-    let result = ""
-    playerSelection = prompt("Rock, paper, scissors: ");
-    playerSelection = playerSelection.toLowerCase();
-    
-    if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-        return result = "Victory";
-    } else if (playerSelection === "rocks" && computerSelection === "paper" || playerSelection ==="scissors" && computerSelection === "rock" || playerSelection === "paper" && computerSelection ==="scissors") {
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-        return result = "Failure";
+function playRound(playerSelection, computerSelection) {  
+    if (playerSelection === "rock" && computerSelection === "scissors" || 
+        playerSelection === "scissors" && computerSelection === "paper" || 
+        playerSelection === "paper" && computerSelection === "rock") {
+          divResults.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+          playerScore += 1;
+          
+    } else if (
+        playerSelection === "rock" && computerSelection === "paper" || 
+        playerSelection ==="scissors" && computerSelection === "rock" || 
+        playerSelection === "paper" && computerSelection ==="scissors") {
+          divResults.textContent= `You lose! ${computerSelection} beats ${playerSelection}`;
+          cpuScore += 1;
     } else  {
-        console.log(`You tie. You picked ${playerSelection} and the computer picked ${computerSelection}`);
-        return result = "Tied";
+        divResults.textContent = `You tie. You picked ${playerSelection} and the computer picked ${computerSelection}`;
+        
     }
 }
 
-function game() {
-    let playerWin = 0
-    let computerWin = 0
-    for (let i = 0; i < 5; i++) {
-        let result = gamePlay()
-        console.log(result);
-        if (result === "Victory") {
-            playerWin += 1;
-        } else if ( result === "Failure") {
-            computerWin += 1;
+let playerScore = 0
+let cpuScore = 0
+
+const container = document.querySelector('#container');
+const btnRock = document.createElement('button');
+const btnPaper = document.createElement('button');
+const btnScissors = document.createElement('button');
+container.appendChild(btnPaper);
+container.appendChild(btnRock);
+container.appendChild(btnScissors);
+
+const divResults = document.createElement('div');
+container.appendChild(divResults);
+
+const divScore = document.createElement('div')
+container.appendChild(divScore);
+
+const playerScoreDiv = document.createElement('div');
+const cpuScoreDiv = document.createElement('div');
+
+divScore.appendChild(playerScoreDiv);
+divScore.appendChild(cpuScoreDiv);
+
+const h2Player = document.createElement('h2');
+const playerScoreResult = document.createElement('div');
+h2Player.textContent = "Player Score";
+playerScoreDiv.appendChild(h2Player);
+playerScoreDiv.appendChild(playerScoreResult);
+
+const h2Cpu = document.createElement('h2');
+const cpuScoreResult = document.createElement('div');
+h2Cpu.textContent = "CPU Score";
+cpuScoreDiv.appendChild(h2Cpu);
+cpuScoreDiv.appendChild(cpuScoreResult);
+
+const finalScore = document.createElement('div');
+container.appendChild(finalScore);
+
+
+cpuScoreResult.textContent = cpuScore;
+playerScoreResult.textContent = playerScore
+
+let buttons = document.querySelectorAll('button');
+
+btnPaper.textContent = 'paper';
+btnRock.textContent = 'rock';
+btnScissors.textContent = 'scissors';
+
+buttons.forEach((button) =>{
+    button.addEventListener("click", () => {
+        let playerSelection = button.textContent
+        playRound(playerSelection, computerPlay());
+
+        cpuScoreResult.textContent = cpuScore;
+        playerScoreResult.textContent = playerScore;
+
+        if (playerScore === 5){
+            finalScore.textContent="You won!"
+            playerScore = 0
+            cpuScore = 0
+        } else if (cpuScore === 5){
+            finalScore.textContent="The CPU won!"
+            playerScore = 0
+            cpuScore = 0
         } else {
-            console.log("Nobody won this round");
+            finalScore.textContent = '';
         }
-        console.log("You have played " + (i+1) + " round(s)");
-        console.log("\n")
-    }
 
-    if (playerWin < computerWin) {
-        console.log("You lost. The computer won " + computerWin + " times. You only won " + playerWin + " times.");
-    } else if (playerWin > computerWin) {
-        console.log("You won! You had " + playerWin + " victories. The computer won " + computerWin + " times.");
-    } else {
-        console.log("You tied against the computer. You won " + playerWin + " matches and the computer won " + computerWin + " matches" )
-    };
-}
 
-console.log(game());
+
+
+    });
+});
+
+
+
+
+
+
+
+
+
